c6ef6f78b01f392fe543affb514117df1ba1e35f
Jacob Brabec <jbrabec@purdue.edu>
2016-07-21 19:09:51 -0400

    Added env file

--- echo.js
+++ echo.js
@@ -1,11 +1,13 @@
-var details = require("./details.json")
 var login = require("facebook-chat-api");
 
+// Load enviornment data
+require('dotenv').config();
+
 // Create simple echo bot
-login({email: details["username"], password: details["password"]}, function callback (err, api) {
+login({email: process.env.BOT_USERNAME, password: process.env.BOT_PASSWORD}, function callback (err, api) {
   if(err) return console.error(err);
   api.listen(function callback(err, message) {
-    if(typeof message.body == "string" && message.body.startsWith("@sadbot echo")) {
+    if(typeof message.body == "string" && message.body.startsWith("@"+process.env.BOT_PREFIX+" echo")) {
       api.sendMessage(message.body.slice(13), message.threadID);
     }
   });
