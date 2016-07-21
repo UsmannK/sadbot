dd2b536b003af81d770536d883bb54a1b8dc88e5
UsmannK <usmann96@gmail.com>
2016-07-21 15:35:04 -0400

    Clean up, add echo script

---
+++ echo.js
@@ -0,0 +1,12 @@
+var details = require("./details.json")
+var login = require("facebook-chat-api");
+
+// Create simple echo bot
+login({email: details["username"], password: details["password"]}, function callback (err, api) {
+  if(err) return console.error(err);
+  api.listen(function callback(err, message) {
+    if(message.body.startsWith("@sadbot echo")) {
+      api.sendMessage(message.body.slice(13), message.threadID);
+    }
+  });
+});
