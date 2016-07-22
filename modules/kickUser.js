7115a9a6ac498019973d8627074b7704d35e4827
Shane DeWael <dewaels@purdue.edu>
2016-07-22 14:39:06 -0700

    kick command

--- modules/kickUser.js
+++ modules/kickUser.js
@@ -1,17 +1,16 @@
 const INVALID = 'INVALID';
 
-function getThreadUsers(threadID, api) {
+function getThreadUsers(threadID, api, callback) {
   api.getThreadInfo(threadID, function(err, info) {
-    console.log(info);
-    return info;
+    callback(info);
   });
 }
 
-function getIDFromName(user, users, threadID, api) {
+function getIDFromName(user, users, threadID, api, callback) {
   api.getUserID(user, function(err, data) {
     for (var id in data) {
       for (var userID in users) {
-        if (id == userID) return userID;
+        if (data[id].userID == users[userID]) callback(users[userID]);
       }
     }
     return INVALID;
@@ -20,35 +19,28 @@ function getIDFromName(user, users, threadID, api) {
 
 function kickUser(userID, threadID, api) {
   api.removeUserFromGroup(userID, threadID, function(err) {
-    if(err) return callback(err);
     return;
   });
 }
 
 function trigger(user, threadID, api) { 
-  console.log("here");
-  var userID;
   getThreadUsers(threadID, api, function(info){
-    console.log(info);
     var nicknames = info.nicknames;
     var users = info.participantIDs;
 
     for (var id in nicknames) {
       if (nicknames[id] == user) {
-        userID = id;
+        kickUser(id, threadID, api);
+        return;
       }
     }
 
-    if (!userID) {
-      userID = getIDFromName(user, users, threadID, api);
-    }
-
-    if (userID != INVALID) {
-      kickUser(userID);
-    }
+    getIDFromName(user, users, threadID, api, function(userID) {
+      if (userID != INVALID) {
+        kickUser(userID, threadID, api);
+      }
+    });
   });
-  
-
 }
 
 module.exports = {
