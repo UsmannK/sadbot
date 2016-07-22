db594910874684c4fbf03e44bfd7384c15b69f64
UsmannK <usmann96@gmail.com>
2016-07-22 14:41:06 -0400

    Add slash commands/testing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ require('./cronjobs.js');
 require('dotenv').config();
 var login = require('facebook-chat-api');
 var commandDescriptions = require('./modules.json');
-
+var prefixLen = 1;
 var commands = commandDescriptions .map(function(cmd) {
   return require('./' + cmd['path']);
 });
@@ -13,8 +13,8 @@ login({email: process.env.BOT_USERNAME, password: process.env.BOT_PASSWORD}, log
 function loginCallback(err, api) {
   if(err) return console.error(err);
   api.listen(function callback(err, message) {
-    if(message && message.body && message.body.startsWith(process.env.BOT_PREFIX)) {
-      var commandString = message.body.slice(process.env.BOT_PREFIX.length);
+    if(message && message.body && isCommand(message.body)) {
+      var commandString = message.body.slice(prefixLen);
       var trigger = commandString.substring(0, commandString.indexOf(' '));
       commands.forEach(function(cmd, index) {
         if(trigger == commandDescriptions[index]['trigger']) {
@@ -25,3 +25,11 @@ function loginCallback(err, api) {
   });
 }
 
+function isCommand(message) {
+  if(process.env.USE_PREFIX == 'true') {
+    prefixLen = process.env.BOT_PREFIX.length + 1;
+    return message.startsWith(process.env.BOT_PREFIX);
+  } else {
+    return message.startsWith('/');
+  }
+}
