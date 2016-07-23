dd7d6fe18c3f26fd71fee96ce6b7ea2cf4221e03
Matthew Ess <mess@purdue.edu>
2016-07-23 04:13:19 -0400

    Update firebase config to work properly

--- main.js
+++ main.js
@@ -10,11 +10,7 @@ var commands = commandDescriptions .map(function(cmd) {
 
 var firebase = require('firebase');
 firebase.initializeApp({
-  serviceAccount: {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-  },
+  serviceAccount: require(process.env.FIREBASE_JSON_LOCATION),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
