642f4ccf63735eadaab523ddd8204091833f5158
Matthew Ess <mess@purdue.edu>
2016-07-23 03:51:51 -0400

    Add minimal firebase setup

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ var commands = commandDescriptions .map(function(cmd) {
   return require('./' + cmd['path']);
 });
 
+var firebase = require('firebase');
+firebase.initializeApp({
+  serviceAccount: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+  },
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
 login({email: process.env.BOT_USERNAME, password: process.env.BOT_PASSWORD}, loginCallback);
 
 function loginCallback(err, api) {
