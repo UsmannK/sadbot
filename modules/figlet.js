6eb762f5073132632b69b16098960e8a6e6d1d21
Jay Hankins <jay.hankins@outlook.com>
2016-07-22 22:32:30 -0400

    Add figlet module

--- modules/figlet.js
+++ modules/figlet.js
@@ -1,16 +1,31 @@
 /**
  *  Name: Figlet
- *  Description: Figlet for SadBot.
- *  Commands:
- *    /figlet String in
- *  Author:
- *    j
+ *  Description: Replies with a figlet of a message.
+ *  Usage: /figlet String in
+ *  Author: j <jay@jayhankins.me>
  */
 
-function trigger(message, api, messageObj) {
+var figlet = require('figlet');
 
+function trigger(message, api, messageObj) {
+  threadID = messageObj.threadID;
+  var figged = '```\n' + makeFig(message) + '```';
+  api.sendMessage(figged, threadID);
+}
+
+function makeFig(message) {
+	return figlet.textSync(message, {
+	    font: 'small'
+	}, function(err, data) {
+	    if (err) {
+	        console.log('Something went wrong...');
+	        console.dir(err);
+	        return;
+	    }
+	    console.log(data);
+	});
 }
 
 module.exports = {
-
+  trigger: trigger
 }
