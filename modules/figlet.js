42881d828da4d84ad60ea3a7529a0ecb3365f815
Jay Hankins <jay.hankins@outlook.com>
2016-07-22 21:51:51 -0400

    Create figlet module skeleton

---
+++ modules/figlet.js
@@ -0,0 +1,16 @@
+/**
+ *  Name: Figlet
+ *  Description: Figlet for SadBot.
+ *  Commands:
+ *    /figlet String in
+ *  Author:
+ *    j
+ */
+
+function trigger(message, api, messageObj) {
+
+}
+
+module.exports = {
+
+}
