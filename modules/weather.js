7a55794a1c3113477a2b0c03dadaa078188e0aad
Kedar <kedar54@gmail.com>
2016-07-26 11:52:38 -0400

    prevent weather crash on invalid location

--- modules/weather.js
+++ modules/weather.js
@@ -1,57 +1,60 @@
 var Forecast = require('forecast');
 var NodeGeocoder = require('node-geocoder');
 var moment = require('moment');
-var options = { provider:'google' };
+var options = { provider: 'google' };
 var geocoder = NodeGeocoder(options);
 var config = require('config');
 var forecast = new Forecast({
-  service: 'forecast.io',
-  key: config.get('forecastIoKey'),
-  units: 'f',
-  cache: true,
-  ttl: {
-    minutes: 30
-  }
+    service: 'forecast.io',
+    key: config.get('forecastIoKey'),
+    units: 'f',
+    cache: true,
+    ttl: {
+        minutes: 30
+    }
 });
 var weatherEmojis = {
-  'clear-day': '☀️',
-  'clear-night': '🌚',
-  'rain': '🌧',
-  'snow': '❄️',
-  'sleet': '🌨',
-  'wind': '💨',
-  'fog': '🌫',
-  'cloudy': '☁️',
-  'partly-cloudy-day': '🌤',
-  'partly-cloudy-night': '☁️'
+    'clear-day': '☀️',
+    'clear-night': '🌚',
+    'rain': '🌧',
+    'snow': '❄️',
+    'sleet': '🌨',
+    'wind': '💨',
+    'fog': '🌫',
+    'cloudy': '☁️',
+    'partly-cloudy-day': '🌤',
+    'partly-cloudy-night': '☁️'
 };
 
 function getEmoji(weather) {
-  return weatherEmojis[weather] ? weatherEmojis[weather] : '';
+    return weatherEmojis[weather] ? weatherEmojis[weather] : '';
 }
 
-function trigger(city, api, message) { 
-  var args = message.body.split(" ");
-  threadID = message.threadID;
-  geocoder.geocode(city, function(err, res) {
-    forecast.get([res[0]['latitude'], res[0]['longitude']], function(err, weather) {
-      var response = "";
-	  if(err) return console.dir(err);
+function trigger(city, api, message) {
+    var args = message.body.split(" ");
+    threadID = message.threadID;
+    if (args[1] === undefined) {
+        return;
+    }
+    geocoder.geocode(city, function(err, res) {
+        if (res === undefined || res.length == 0) { return console.log("err"); }
+        forecast.get([res[0]['latitude'], res[0]['longitude']], function(err, weather) {
+            var response = "";
+            if (err) return console.dir(err);
 
-	  if(args[3] === 'daily') {
-        weather['daily']['data'].forEach(function(day) {
-        	response += moment.unix(day['time']).format("dddd") + ": " + day['summary'] + "\n";
+            if (args[3] === 'daily') {
+                weather['daily']['data'].forEach(function(day) {
+                    response += moment.unix(day['time']).format("dddd") + ": " + day['summary'] + "\n";
+                });
+            } else {
+                var weatherEmoji = getEmoji(weather['currently']['icon']);
+                response = weatherEmoji + " Currently " + Math.floor(weather['currently']['temperature']) + "°F, and " + weather['currently']['summary'].toLowerCase() + ". " + weather['daily']['data'][0]['summary'];
+            }
+            api.sendMessage(response, threadID);
         });
-	  } else {
-      var weatherEmoji = getEmoji(weather['currently']['icon']);
-	  	response = weatherEmoji + " Currently " + Math.floor(weather['currently']['temperature']) + "°F, and "
-          + weather['currently']['summary'].toLowerCase() + ". " + weather['daily']['data'][0]['summary'];
-	  }
-      api.sendMessage(response, threadID);
     });
-  });
 }
 
 module.exports = {
-  trigger: trigger
+    trigger: trigger
 }
