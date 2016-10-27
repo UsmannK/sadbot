122c1b4fb544f58701e98aa78d2cb58a4abb36cf
Shane DeWael <dewaels@purdue.edu>
2016-10-27 17:41:19 -0400

    fix

--- modules/weed.js
+++ modules/weed.js
@@ -4,89 +4,107 @@
  *  Usage: /weed <strain-name>
  */
 
- var cannabis = require('cannabis-reports');
+var cannabis = require('cannabis-reports');
+var giphy = require('giphy-api')();
 
- function trigger(message, api, messageObj) {
- 	threadID = messageObj.threadID;
- 	search(message, function(ucpc){
- 		console.log(ucpc);
- 		buildResult(ucpc, function(msg){
- 			api.sendMessage(msg, threadID);
- 		});
- 	});
+const INVALID = 'I couldn\'t find that strain :/';
 
- }
+function trigger(message, api, messageObj) {
+	threadID = messageObj.threadID;
+	search(message, function(ucpc){
+		api.sendTypingIndicator(threadID, function() {
+	 		buildResult(ucpc, function(msg){
+	 			api.sendMessage(msg, threadID);
+	 		});
+ 		})
+	});
+}
 
- function search(query, callback) {
- 	cannabis.Strain
- 	.search(query)
- 	.then(function(res){
- 		var i;
- 		for (i in res) {
- 			if ((res[i].reviews.count > 0) && (res[i].ucpc)) {
- 				break;
- 			}
- 		}
- 		if (res[i].ucpc) {
- 			callback(res[i].ucpc);
- 		} else {
- 			callback(null);
- 		}
- 	})
- 	.catch(function(err){
- 		console.log(err);
- 	})
- }
+function search(query, callback) {
+	cannabis.Strain
+	.search(query)
+	.then(function(res){
+		var i;
+		for (i in res) {
+			if ((res[i].reviews.count > 0) && (res[i].ucpc)) {
+				break;
+			}
+		}
+		if (res[i].ucpc) {
+			callback(res[i].ucpc);
+		} else {
+			callback(null);
+		}
+	})
+	.catch(function(err){
+		callback(INVALID);
+	})
+}
+function buildResult(ucpc, callback) {
+	if (ucpc == INVALID) {
+		var msg = {
+			body: INVALID,
+			url: ''
+		};
+		giphy.random('weed', function(err, res) {
+	    if (err) return callback(INVALID);
+	    	msg.url = res.data.url;
+	    	callback(msg);
+	  	});
+	}
 
- function buildResult(ucpc, callback) {
- 	var strain = '```\n';
- 	getStrain(ucpc, function(info) {
- 		strain += info.name;
- 		if (info.genetics.names) {
- 			strain += '(' + info.genetics.names + ')\n\n'
- 		} else {
- 			'\n\n';
- 		}
+	var strain = '```\n';
+	getStrain(ucpc, function(info) {
+		strain += info.name;
+		if (info.genetics.names) {
+			strain += ' (' + info.genetics.names + ')\n\n'
+		} else {
+			'\n\n';
+		}
+		getEffects(ucpc, function(effects) {
+			for (var effect in effects) {
+				if (effects.hasOwnProperty(effect)) {
+					var num = twoDecimals(effects[effect]);
+					if ((effect != 'reviews') && (num != '0.00')) {
+						strain += capitalizeFirst(effect).replace('_', ' ') + ': ' + num + '\n';
+					}
+				}
+			}
+			callback(strain);
+		});
+	});
+}
 
- 		getEffects(ucpc, function(effects) {
- 			for (var effect in effects) {
- 				if (effects.hasOwnProperty(effect)) {
- 					if (effect != 'reviews') {
- 						strain += capitalizeFirst(effect) + ': ' + effects[effect] + '\n';
- 					}
- 				}
- 			}
- 			callback(strain);
- 		});
- 	});
- }
+function getStrain(ucpc, callback) {
+	cannabis.Strain
+	.strain(ucpc)
+	.then(function(res){
+		callback(res);
+	})
+	.catch(function(err){
+		callback(null);
+	})
+}
 
- function getStrain(ucpc, callback) {
- 	cannabis.Strain
- 	.strain(ucpc)
- 	.then(function(res){
- 		callback(res);
- 	})
- 	.catch(function(err){
- 		callback(null);
- 	})
- }
+function getEffects(ucpc, callback) {
+	cannabis.Strain
+	.effectsFlavors(ucpc)
+	.then(function(res){
+		callback(res);
+	})
+	.catch(function(err){
+		callback(null);
+	})
+}
 
- function getEffects(ucpc, callback) {
- 	cannabis.Strain
- 	.effectsFlavors(ucpc)
- 	.then(function(res){
- 		callback(res);
- 	})
- 	.catch(function(err){
- 		callback(null);
- 	})
- }
+function capitalizeFirst(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
- function capitalizeFirst(str) {
- 	return str.charAt(0).toUpperCase() + str.slice(1);
- }
+function twoDecimals(num) {
+	return parseFloat(Math.round(num * 100) / 100).toFixed(2);
+}
 
- module.exports = {
- 	trigger: trigger
- }
+module.exports = {
+	trigger: trigger
+}
