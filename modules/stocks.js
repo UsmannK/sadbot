7b1068a9c124b3608dfd827f9bc1668c82a51ed3
Kedar V <kedar54@gmail.com>
2016-10-30 13:22:36 -0400

    hotfix stocks

--- modules/stocks.js
+++ modules/stocks.js
@@ -9,12 +9,11 @@ var yfinance = require('yfinance');
 function trigger(search, api, messageObj) {
 	var args = messageObj.body.split(" ");
 	var threadID = messageObj.threadID;
-	if(typeof args[2] == 'undefined') {
+	if(typeof args[1] == 'undefined') {
 		console.error('bad arguments');
 	}
-	console.log(args[2]);
 	var end = api.sendTypingIndicator(threadID, function() {
-		yfinance.getQuotes(args[2], function (err, data) {
+		yfinance.getQuotes(args[1], function (err, data) {
 			if(err) {
 				console.error(err);
 			} else if(data[0].Ask != null) {
