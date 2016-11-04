48174a5fa3a0394aa66e3863d8ad718eb2726ed2
Shane DeWael <dewaels@purdue.edu>
2016-11-03 23:03:03 -0400

    add weather tomorow and clean up args

--- modules/weather.js
+++ modules/weather.js
@@ -31,32 +31,39 @@ function getEmoji(weather) {
 }
 
 function trigger(city, api, message) {
-    var sendCity = city;
-    var args = message.body.split(" ");
-    threadID = message.threadID;
+    var args = city.split(",");
+    var threadID = message.threadID;
     if (city === "") {
         sendCity = "West Lafayette";
     }
-    geocoder.geocode(sendCity, function(err, res) {
+    var sendCity = args[0];
+    if (sendCity == null) {
+        sendCity = city;
+    }
+    geocoder.geocode(sendCity.trim(), function(err, res) {
         if (res === undefined || res.length == 0) { return console.error("could not geocode"); }
         forecast.get([res[0]['latitude'], res[0]['longitude']], function(err, weather) {
             var response = "";
             if (err) return console.dir(err);
-
-            if (args[3] === 'daily') {
-                weather['daily']['data'].forEach(function(day) {
-                    response += moment.unix(day['time']).format("dddd") + ": " + day['summary'] + "\n";
-                });
-            } else {
+            if (args[1] == null) {
                 var weatherEmoji = getEmoji(weather['currently']['icon']);
                 response = weatherEmoji + " Currently " + Math.floor(weather['currently']['temperature']) + "°F, and " + weather['currently']['summary'].toLowerCase() + ". " + weather['daily']['data'][0]['summary'];
-                console.log(weather['currently']['temperature'] > 70);
                 if(weather['currently']['temperature'] > 70) {
                     var num = (weather['currently']['humidity']*100) + "%";
                     response += " Humidity: " + num;
                 }
+            } else if (args[1].trim() === 'daily') {
+                var weatherEmoji;
+                weather['daily']['data'].forEach(function(day) {
+                    response += moment.unix(day['time']).format("dddd") + ": " + getEmoji(day['icon']) + " " + day['summary'] + "\n\n";
+                });
+            } else if (args[1].trim() == 'tomorrow') {
+                var day = weather['daily']['data'][1];
+                var weatherEmoji = getEmoji(day['icon']);
+                response += weatherEmoji + " " + day['summary'];
+            } else {
+                response = "Couldn't find weather :/";
             }
-            console.log(response);
             api.sendMessage(response, threadID);
         });
     });
