122e0201be553c77bf2ba398089b73f8e51030d0
Shane DeWael <dewaels@purdue.edu>
2016-11-04 14:08:08 -0400

    fix img delete for pokdex

--- modules/pokedex.js
+++ modules/pokedex.js
@@ -21,16 +21,16 @@ function getInfo(msg, api) {
 		api.sendTypingIndicator(threadID, function() {
 			getPokemonMoveByName(msg[1], function(msg){
 				api.sendMessage(msg, threadID);
-				if (close != null) {
-					fs.unlink(close);
-				}
 			})
 		})
 			
 	} else {
 		api.sendTypingIndicator(threadID, function() {
-			getPokemonName(msg[0], function(msg) {
+			getPokemonName(msg[0], function(msg, close) {
 				api.sendMessage(msg, threadID);
+				if (close != null) {
+					fs.unlink(close);
+				}
 			})
 		})
 	}
