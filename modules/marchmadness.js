be4bf107e4cbb4724ef5d432952fe8881bddc13b
Kedar V <kedar54@gmail.com>
2017-03-16 12:45:54 -0500

    add live command to mm

--- modules/marchmadness.js
+++ modules/marchmadness.js
@@ -3,6 +3,7 @@ var superagent = require('superagent');
 
 function trigger(message, api, messageObj) {
   var threadID = messageObj.threadID;
+  var options = message.split(" ");
   var end = api.sendTypingIndicator(threadID, function() {
     superagent
       .get("http://www.cbssports.com/college-basketball/scoreboard/")
@@ -11,19 +12,30 @@ function trigger(message, api, messageObj) {
           console.error("Error getting ESPN HTML");
         } else {
           $ = cheerio.load(res.text);
-          var string = "";
-          $('.single-score-card').each(function(i, elm) {
-            console.log("-------");
-            if($(this).find('.team').find('a').first().length > 0) {
-              var broadcaster = "CBS";
-              if($(this).find('.broadcaster').children().length == 1) {
-                var broadcaster = $(this).find('.broadcaster').text().trim().replace(/ /g,'').substring(10);
+          if(options.length == 0) {
+            var string = "";
+            $('.single-score-card').each(function(i, elm) {
+              if($(this).find('.team').find('a').first().length > 0) {
+                var broadcaster = "CBS";
+                if($(this).find('.broadcaster').children().length == 1) {
+                  var broadcaster = $(this).find('.broadcaster').text().trim().replace(/ /g,'').substring(10);
+                }
+                string += $(this).find('.team').find('a').first().text().trim();
+                string += " vs " + $(this).find('.team').find('a').last().text().trim();
+                string += " @ " + $(this).find('.game-status').find('span').text().trim() + " on " + broadcaster + "\n";
               }
-              string += $(this).find('.team').find('a').first().text().trim();
-              string += " vs " + $(this).find('.team').find('a').last().text().trim();
-              string += " @ " + $(this).find('.game-status').find('span').text().trim() + " on " + broadcaster + "\n";
-            }
-          });
+            });
+          } else if(options.length == 1 && options[0] === "live") {
+            var string = "";
+            $('.ingame').each(function(i, elm) {
+              var status = ($(this).find('.emphasis').text().trim());
+              var score_team_one = $(this).find('tr').find('td').eq(3).text()
+              var score_team_two = $(this).find('tr').find('td').eq(7).text()
+              string += $(this).find('.team').find('a').first().text().trim() + " ("+score_team_one+")";
+              string += " vs " + $(this).find('.team').find('a').last().text().trim() +" ("+score_team_two+")";
+              string += " - " + status + "\n";
+            });
+          }
           api.sendMessage(string, threadID);
         }
         end();
