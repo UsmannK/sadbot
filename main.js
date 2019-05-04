81dd27c517470b80e4817d80e09ed45993be16b6
kedarv <kedar54@gmail.com>
2019-05-04 11:34:37 -0700

    add some thanking

--- main.js
+++ main.js
@@ -9,43 +9,54 @@ const prefixLen = 1;
 
 // return module that handles a command
 const commands = commandDescriptions.map(function(cmd) {
-  return require('./' + cmd['path']);
+  return require(`./${cmd.path}`);
 });
 
+const freeformTriggers = ['thanks robby', 'thanks sadbot', 'thanks bot'];
+const thanks = [
+  'you got it!',
+  "you're welcome friendo",
+  '_beep boop_',
+  '_<3_',
+  'cheers 🍻',
+  'np bud'
+];
+
 // parse messages for handling
-(async() => {
+(async () => {
   const api = await login(config.get('botUsername'), config.get('botPassword'));
   api.on('message', message => {
-    if(isCommand(message.message)) {
+    if (isCommand(message.message)) {
       const commandString = message.message.slice(prefixLen);
       const trigger = commandString.substring(0, endOfCmd(commandString));
       commands.forEach(function(cmd, index) {
-        if(trigger === commandDescriptions[index]['trigger']) {
-          const args = commandString.slice(trigger.length+1) || '';
+        if (trigger === commandDescriptions[index].trigger) {
+          const args = commandString.slice(trigger.length + 1) || '';
           cmd.trigger(args, api, message);
         }
       });
+    } else if (freeformTriggers.some(word => message.message.includes(word))) {
+      api.sendMessage(message.threadId, thanks[Math.floor(thanks.length * Math.random())]);
     }
   });
 })();
 
 // determine the end of the command
 function endOfCmd(cmd) {
-  if(cmd.indexOf(' ') > 0) {
+  if (cmd.indexOf(' ') > 0) {
     return cmd.indexOf(' ');
-  } else {
-    return cmd.length;
   }
+  return cmd.length;
 }
 
 // determine if a received message is a command
 function isCommand(message) {
-  if (!(message)) {
+  if (!message) {
     return false;
-  } else if (config.get('usePrefix')) {
+  }
+  if (config.get('usePrefix')) {
     prefixLen = config.get('botPrefix').length + 1;
     return message.startsWith(config.get('botPrefix'));
-  } else {
-    return message.startsWith('/');
   }
+  return message.startsWith('/');
 }
